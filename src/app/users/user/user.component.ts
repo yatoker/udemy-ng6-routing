3e9ea41ebadb559fd5aa44cf9ba400a6559e6ea1
Yusuf Andre Toker <yusufandre@gamil.com>
2018-09-04 13:24:24 +0300

    unsubscribe observable when component is destroyed.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+
+  user: { id: number, name: string };
+  paramsSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,10 +21,17 @@ export class UserComponent implements OnInit {
     }
 
     // this.route.params is an observable that means once subscribed to it, whenever the route is reached, the callback func. will be executed.
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'],
-      this.user.name = params['name']
+        this.user.name = params['name']
     });
+  }
+
+  ngOnDestroy(): void {
+    //Normally, observable subscription won't be removed when a component is destroyed. 
+    //Subscription information is saved in the memory and whenever a destroyed and subscribed component is reinited, it gets subscribed again.
+    //Calling unsubscribe on component destroy method does the job when ubsubscription needed.
+    this.paramsSubscription.unsubscribe();
   }
 
 }
